14bb5a4480ebd3cb19677558c0400afee98f5db1
bluesamsam <103419843+melosamsam@users.noreply.github.com>
2024-02-12 14:56:33 +0100

    Creating Database suite

--- app/src/main/java/com/example/tourism_app/MainActivity.kt
+++ app/src/main/java/com/example/tourism_app/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.tourism_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.DatabaseReference
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true)
     }
 }
