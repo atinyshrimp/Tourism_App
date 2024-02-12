9198e9217f52c5d372808cd79f0fc8f7ff5e999c
Ali <alibennouna003@gmail.com>
2024-02-12 15:31:32 +0100

    Login/Register base design

--- app/src/main/java/com/example/tourism_app/Login.kt
+++ app/src/main/java/com/example/tourism_app/Login.kt
@@ -2,11 +2,9 @@ package com.example.tourism_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.database.ktx.database
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.DatabaseReference
 
-class MainActivity : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
