762bf38cc2b3fbb6f9924513b514631afeb4b037
Joyce <lianajoycemarie@gmail.com>
2024-02-16 23:43:31 +0100

    Cleaned Home Fragment implementation

--- app/src/main/java/com/example/tourism_app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/tourism_app/ui/home/HomeFragment.kt
@@ -6,15 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.tourism_app.Activity
-import com.example.tourism_app.ActivityRecyclerAdapter
-import com.example.tourism_app.Category
-import com.example.tourism_app.CategoryAdapter
-import com.example.tourism_app.R
 import com.example.tourism_app.databinding.FragmentHomeBinding
-import com.google.android.material.tabs.TabLayout
 
 class HomeFragment : Fragment() {
 
@@ -24,47 +16,20 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    private lateinit var activityRecyclerView : RecyclerView
-    private lateinit var activityList : ArrayList<Activity>
-
-    private lateinit var categoryList : ArrayList<Category>
-    private lateinit var categoryRecyclerView : RecyclerView
-
-    private lateinit var tabLayout : TabLayout
+    private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
+        homeViewModel =
             ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        tabLayout = binding.activityTabs
-        tabLayout.getTabAt(1)?.select()
-        // activityLayoutListenerInit()
 
-        // getting values for the Activity recycler view
-        activityRecyclerView = binding.activityList
-        activityRecyclerView.layoutManager = LinearLayoutManager(this.context,
-                                                                LinearLayoutManager.HORIZONTAL,
-                                                                false)
-
-        // initializing the list of activities
-        activityList = arrayListOf()
-        getActivityData()
-
-        // getting values for the Category recycler view
-        categoryRecyclerView = binding.categoryList
-        categoryRecyclerView.layoutManager = LinearLayoutManager(this.context,
-            LinearLayoutManager.HORIZONTAL,
-            false)
-
-        // initializing the list of categories
-        categoryList = arrayListOf()
-        getCategoryData()
+        homeViewModel.setupViews(binding, this)
 
         return root
     }
@@ -72,56 +37,5 @@ class HomeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    private fun getActivityData() {
-
-        // test Activity elements until Firebase liaison
-        val activities = listOf(
-            Activity(name="Louvre", address="8 rue Sainte-Anne, 75001 Paris",
-            description = "Experience the Louvre, the world's largest and most visited art museum, nestled in the heart of Paris. Home to over 35,000 works of art spanning from ancient civilizations to the 19th century, this iconic institution showcases the pinnacle of human creativity. Marvel at renowned masterpieces, including Leonardo da Vinci's Mona Lisa, the ancient Greek sculpture Venus de Milo, and the striking Winged Victory of Samothrace. The Louvre's architectural grandeur, from the medieval fortress to the glass pyramid entrance, adds to the allure of this cultural gem. Dive into a rich tapestry of history and artistry as you explore the Louvre's vast collections, making it an essential destination for any art and history enthusiast.",
-            transport = null, hours="09:00 - 18:00", category="Museum"),
-            Activity(name="Parc des Buttes-Chaumont", address="1 Rue Botzaris, 75019 Paris",
-            description = "A beautiful public park with hills, bridges, and a lake.",
-            transport = null, hours="07:00 - 22:00", category="Garden")
-        )
-
-        for (element in activities) {
-            activityList.add(element)
-        }
-
-        activityRecyclerView.adapter = ActivityRecyclerAdapter(activityList)
-    }
-
-    private fun getCategoryData() {
-        val categories = listOf(
-            Category(resources.getStringArray(R.array.categories)[0], R.drawable.category_aesthetics),
-            Category(resources.getStringArray(R.array.categories)[1], R.drawable.category_event),
-            Category(resources.getStringArray(R.array.categories)[2], R.drawable.category_garden),
-            Category(resources.getStringArray(R.array.categories)[3], R.drawable.category_museum)
-        )
-
-        for (element in categories) {
-            categoryList.add(element)
-        }
-
-        categoryRecyclerView.adapter = CategoryAdapter(categoryList)
-    }
-
-    private fun activityLayoutListenerInit(){
-        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                // Handle tab selection, filter the list accordingly
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-                // Handle tab unselection if needed
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-                // Handle tab reselection if needed
-            }
-        })
-
     }
 }
--- app/src/main/java/com/example/tourism_app/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/tourism_app/ui/home/HomeViewModel.kt
@@ -1,13 +1,97 @@
 package com.example.tourism_app.ui.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tourism_app.Activity
+import com.example.tourism_app.ActivityRecyclerAdapter
+import com.example.tourism_app.Category
+import com.example.tourism_app.CategoryAdapter
+import com.example.tourism_app.R
+import com.example.tourism_app.databinding.FragmentHomeBinding
+import com.google.android.material.tabs.TabLayout
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    fun setupViews(binding: FragmentHomeBinding, fragment: HomeFragment) {
+        // Access UI components and perform setup
+        val tabLayout = binding.activityTabs
+        tabLayout.getTabAt(1)?.select()
+
+        // getting values for the Activity recycler view
+        val activityRecyclerView = binding.activityList
+        activityRecyclerView.layoutManager = LinearLayoutManager(fragment.context,
+            LinearLayoutManager.HORIZONTAL,
+            false)
+
+        // initializing the list of activities
+        val activityList = arrayListOf<Activity>()
+        getActivityData(activityList, activityRecyclerView)
+
+        // getting values for the Category recycler view
+        val categoryRecyclerView = binding.categoryList
+        categoryRecyclerView.layoutManager = LinearLayoutManager(fragment.context,
+            LinearLayoutManager.HORIZONTAL,
+            false)
+
+        // initializing the list of categories
+        val categoryList = arrayListOf<Category>()
+        getCategoryData(fragment, categoryList, categoryRecyclerView)
+
+        // setupTabs(tabLayout)
     }
-    val text: LiveData<String> = _text
+
+    fun setupTabs(tabLayout: TabLayout) {
+        // Setup listeners or event handling if needed
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                // Handle tab selection, filter the list accordingly
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                // Handle tab unselection if needed
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                // Handle tab reselection if needed
+            }
+        })
+
+    }
+
+    private fun getActivityData(activityList: ArrayList<Activity>, activityRecyclerView: RecyclerView) {
+
+        // test Activity elements until Firebase liaison
+        val activities = listOf(
+            Activity(name="Louvre", address="8 rue Sainte-Anne, 75001 Paris",
+                description = "Experience the Louvre, the world's largest and most visited art museum, nestled in the heart of Paris. Home to over 35,000 works of art spanning from ancient civilizations to the 19th century, this iconic institution showcases the pinnacle of human creativity. Marvel at renowned masterpieces, including Leonardo da Vinci's Mona Lisa, the ancient Greek sculpture Venus de Milo, and the striking Winged Victory of Samothrace. The Louvre's architectural grandeur, from the medieval fortress to the glass pyramid entrance, adds to the allure of this cultural gem. Dive into a rich tapestry of history and artistry as you explore the Louvre's vast collections, making it an essential destination for any art and history enthusiast.",
+                transport = null, hours="09:00 - 18:00", category="Museum"),
+            Activity(name="Parc des Buttes-Chaumont", address="1 Rue Botzaris, 75019 Paris",
+                description = "A beautiful public park with hills, bridges, and a lake.",
+                transport = null, hours="07:00 - 22:00", category="Garden")
+        )
+
+        for (element in activities) {
+            activityList.add(element)
+        }
+
+        activityRecyclerView.adapter = ActivityRecyclerAdapter(activityList)
+    }
+
+    private fun getCategoryData(fragment: HomeFragment,
+                                categoryList: ArrayList<Category>,
+                                categoryRecyclerView: RecyclerView) {
+        val categories = listOf(
+            Category(fragment.resources.getStringArray(R.array.categories)[0], R.drawable.category_aesthetics),
+            Category(fragment.resources.getStringArray(R.array.categories)[1], R.drawable.category_event),
+            Category(fragment.resources.getStringArray(R.array.categories)[2], R.drawable.category_garden),
+            Category(fragment.resources.getStringArray(R.array.categories)[3], R.drawable.category_museum)
+        )
+
+        for (element in categories) {
+            categoryList.add(element)
+        }
+
+        categoryRecyclerView.adapter = CategoryAdapter(categoryList)
+    }
 }
