baee16f94a98db01b538ad331e728685f125a043
bluesamsam <103419843+melosamsam@users.noreply.github.com>
2024-03-05 15:03:57 +0100

    Categories picture are now imported from Firebase storage

--- app/src/main/java/com/example/tourism_app/data/Category.kt
+++ app/src/main/java/com/example/tourism_app/data/Category.kt
@@ -3,8 +3,5 @@ package com.example.tourism_app.data
 import androidx.annotation.DrawableRes
 
 data class Category (
-    val name : String ?= null,
-
-    // have to change to the actual way to get an image from Firebase
-    @DrawableRes val imageId : Int ?= null
+    val name : String ?= null
 )
