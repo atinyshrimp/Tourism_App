f73547482e5c1ddacdf128901bf1f52962b2d78f
AduDeVinci <auriane.dupin@edu.devinci.fr>
2024-03-11 19:36:23 +0100

    added marker

--- app/src/main/java/com/example/tourism_app/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/tourism_app/ui/dashboard/DashboardFragment.kt
@@ -1,21 +1,25 @@
 package com.example.tourism_app.ui.dashboard
 
 import android.os.Bundle
-
 import android.view.LayoutInflater
-import androidx.lifecycle.ViewModelProvider
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.tourism_app.R
 import com.example.tourism_app.databinding.FragmentDashboardBinding
-import com.example.tourism_app.ui.home.HomeViewModel
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.CancelableCallback
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 
 class DashboardFragment (private val user: String) : Fragment(), OnMapReadyCallback {
@@ -54,9 +58,32 @@ class DashboardFragment (private val user: String) : Fragment(), OnMapReadyCallb
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Paris and move the camera
-        val paris = LatLng(48.85667037963867, 2.3521103858947754)
-        mMap.addMarker(MarkerOptions().position(paris).title("Marker in Paris"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(paris))
+        //move the camera
+        val paris = LatLng(48.85167037963867, 2.3421103858947756)
+        val zoom = 12f
+
+        val markerView = (requireContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(R.layout.marker_layout, null)
+        val cardview= markerView.findViewById<CardView>(R.id.marker_or)
+        val bmp=Bitmap.createScaledBitmap(viewToBmp(cardview)!!, cardview.width, cardview.height, false)
+        val iconOk=BitmapDescriptorFactory.fromBitmap(bmp)
+
+        googleMap.addMarker(MarkerOptions().position(paris).icon(iconOk))
+        mMap.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(paris, zoom),
+            2000,
+            object : CancelableCallback {
+                override fun onFinish() {}
+                override fun onCancel() {}
+            })
     }
+
+    private fun viewToBmp(view: View): Bitmap?{
+        view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
+        val bitmap= Bitmap.createBitmap(view.measuredWidth, view.measuredHeight, Bitmap.Config.ARGB_8888)
+        val canvas=Canvas(bitmap)
+        view.layout(0,0,view.measuredWidth, view.measuredHeight)
+        view.draw(canvas)
+        return bitmap
+    }
+
 }
